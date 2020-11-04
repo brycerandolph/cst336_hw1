69a35e7c6409e1e727299caf2ec35d90edb40f53
brycerandolph <brycerandolph75@gmail.com>
2020-11-04 07:26:41 +0000

    dfgfg

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title> Different Types of Storage </title>
+       <link href="css/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+    
+    <body>
+        <header>
+            <h1>Computer Data Storage Technologies</h1>
+        </header>
+        <hr />
+      <nav>
+            <a style="color: navy;" href="index.html">Home</a>
+            <a href="hdd.html">HDD</a>
+            <a href="ssd.html">SSD</a>
+            <a href="sshd.html">SSHD</a>
+            
+        </nav>
+    
+        <br /><br />
+        
+        <main> 
+            <figure>
+                  
+                 <img class = one src="img/trio.webp" alt="Picture of Three types of memory"/> 
+                  <figcaption>As solid state flash storage becomes more accesible, where do hard drives stand?</figcaption>  
+            </figure>
+        
+            <div id="paragraph">
+                 <br />
+                
+                <p>The purpose of this website is to present several different storage
+                technologies that can be utilized by a computer. For decades, the hard disk drive stood as the most common type of storage to install in your computer. 
+                We will take a look at two other methods of storing a computer's data, specifically, the operating system. Flash memory has created both hybrid 
+                hard/flash drives, as well as fully flash memory storage devices called SSDs. The two categories discussed will be the hardware involved and the speeds at which each drive can transfer data.</p>
+             
+                
+                
+            </div>
+        </main>
+        
+        <footer>
+            <hr>
+            
+            CST336: Internet Programming Homework 1. 2020&copy; Randolph <br />
+            <b>Sources:</b>
+            <a href="https://tekhattan.com/blog/hardware/ssd-vs-hdd-speed-lifespan-and-reliability/">Tekhatten</a>
+            <a href="https://www.thecomputercellar.com/hard-drives-explained/">The Computer Cellar</a>
+            <a href="https://www.pcmag.com/news/ssd-vs-hdd-whats-the-difference">PcMag</a>
+        </footer>
+    </body>
+
+
+</html>
+
+
+
+
